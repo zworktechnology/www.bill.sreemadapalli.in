4fc4ac6fa1401bb6b62f01d04dde163ecabbc9aa
Zwork Technology <69674346+zworktechnology@users.noreply.github.com>
2024-03-15 21:15:46 +0530

    Update print.blade.php

--- resources/views/page/backend/sales/print.blade.php
+++ resources/views/page/backend/sales/print.blade.php
@@ -272,6 +272,7 @@
                         {{-- <div class="tm_list_title">P.No :</div> --}}
                         <div class="tm_list_desc" style="font-weight:800;color:black;">{{ $phoneno }}</div>
                     </li>
+                    <li></li>
                     <li class="text-right">
                         {{-- <div class="tm_list_title">Address :</div> --}}
                         <div class="tm_list_desc" style="font-weight:800;font-size:15px;color:black;">{{ $name }}</div>
